10a7fa1f3bfec6f383aa29f047ea10d5d8615b2b
Gaurav Shankar <gauravshankar.can@gmail.com>
2022-04-08 21:29:08 -0400

    basic devops

--- ci/setup-minikube.sh
+++ ci/setup-minikube.sh
@@ -4,6 +4,7 @@ cd chart
 helm upgrade --install trucker .  --namespace=trucker --create-namespace 
 cd ..
 
+cp sshprivate.key ../datatrucker_api/app/keys/sshprivate.key 
 
 echo "creating temp space"
 mkdir -p /tmp/trucker
